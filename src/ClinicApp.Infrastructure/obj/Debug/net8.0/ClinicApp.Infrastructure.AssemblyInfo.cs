bf72f634c367bb8599bda249f08b23c37f0530a7
akashhari-olivo <akashharim193@gmail.com>
2026-06-23 10:36:45 +0530

    Backend deployment

--- src/ClinicApp.Infrastructure/obj/Debug/net8.0/ClinicApp.Infrastructure.AssemblyInfo.cs
+++ src/ClinicApp.Infrastructure/obj/Debug/net8.0/ClinicApp.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed61c28dd90ff3c77d6dc31642613dec45192376")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicApp.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
